895c732b264ff171def70c9aaa3b4ae040c0971b
hong <hsw5971@naver.com>
2024-05-08 15:40:55 +0900

    chore: router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Landing from '@pages/Landing/Landing';
+import Login from '@pages/Login/Login';
+import SignUp from '@pages/SignUp/SignUp';
+import SearchVs from '@pages/SearchVs/SearchVs';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path={'/'} element={<Landing />} />
+        <Route path={'/login'} element={<Login />} />
+        <Route path={'/signup'} element={<SignUp />} />
+        <Route path={'/search-vs'} element={<SearchVs />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Landing() {
+  return (
+    <div>
+      landing
+      <Link to="/login">aaaa</Link>
+      <Link to="/signup">aaaa</Link>
+    </div>
+  );
+}
+
+export default Landing;
